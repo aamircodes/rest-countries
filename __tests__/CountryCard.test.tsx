f67c78145f39b473c56791b35f63936be5c109a4
aamir <aamirp1@outlook.com>
2023-06-21 13:22:41 +0100

    add unit tests for CountryCard

---
+++ __tests__/CountryCard.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import CountryCard from '@/app/components/CountryCard';
+
+describe('CountryCard', () => {
+  const mockCountry = {
+    flags: {
+      png: 'https://example.com/flag.png',
+      alt: 'Country Flag',
+    },
+    name: { common: 'mockCountry' },
+    capital: ['mockCapital'],
+    region: 'RegionName',
+    population: 123456,
+  };
+
+  test('renders correctly when all properties are defined', () => {
+    render(<CountryCard country={mockCountry} />);
+
+    expect(screen.getByRole('img')).toHaveAttribute(
+      'src',
+      mockCountry.flags.png
+    );
+    expect(screen.getByRole('img')).toHaveAttribute(
+      'alt',
+      mockCountry.flags.alt
+    );
+    expect(screen.getByText('mockCountry')).toBeInTheDocument();
+    expect(screen.getByText('Capital:')).toBeInTheDocument();
+    expect(screen.getByText('mockCapital')).toBeInTheDocument();
+    expect(screen.getByText('Region:')).toBeInTheDocument();
+    expect(screen.getByText('RegionName')).toBeInTheDocument();
+    expect(screen.getByText('Population:')).toBeInTheDocument();
+    expect(screen.getByText('123,456')).toBeInTheDocument();
+  });
+
+  test('renders correctly when capital is undefined', () => {
+    const countryWithoutCapital = { ...mockCountry, capital: undefined };
+    render(<CountryCard country={countryWithoutCapital} />);
+
+    expect(screen.queryByText('Capital:')).not.toBeInTheDocument();
+  });
+
+  test('renders correctly when region is undefined', () => {
+    const countryWithoutRegion = { ...mockCountry, region: undefined };
+    render(<CountryCard country={countryWithoutRegion} />);
+
+    expect(screen.queryByText('Region:')).not.toBeInTheDocument();
+  });
+
+  test('renders correctly when population is undefined', () => {
+    const countryWithoutPopulation = { ...mockCountry, population: undefined };
+    render(<CountryCard country={countryWithoutPopulation} />);
+
+    expect(screen.queryByText('Population:')).not.toBeInTheDocument();
+  });
+});
